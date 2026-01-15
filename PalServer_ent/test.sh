cc31774e851a06a8795091dc6b3c9998c130b133
selim <your@email.com>
2026-01-15 13:36:25 +0900

    install

--- PalServer_ent/test.sh
+++ PalServer_ent/test.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 BASE_DIR=$(pwd)
 export PALSERVER_BASE_DIR="$BASE_DIR"
-echo " => docker compose -f UI/docker-compose.yml down  --remove-orphans"
-docker compose -f UI/docker-compose.yml down
+echo " => docker compose -f $BASE_DIR/UI/docker-compose.yml down  --remove-orphans"
+docker compose -f $BASE_DIR/UI/docker-compose.yml down
 
-echo " => docker compose -f UI/docker-compose.yml build --no-cache "
-docker compose -f UI/docker-compose.yml build --no-cache 
+echo " => docker compose -f $BASE_DIR/UI/docker-compose.yml build --no-cache "
+docker compose -f $BASE_DIR/UI/docker-compose.yml build --no-cache
 
-echo " => docker compose -f UI/docker-compose.yml up -d --force-recreate"
-docker compose -f UI/docker-compose.yml up -d
-
- 
+echo " => docker compose -f $BASE_DIR/UI/docker-compose.yml up -d --force-recreate"
+docker compose -f $BASE_DIR/UI/docker-compose.yml up -d
 
 echo " => curl -vf https://서버IP:8443/admin/"
 curl -vk https://192.168.1.150:8443/admin/
